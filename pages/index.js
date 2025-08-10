0033b339ff6ef804cb90aad4d9b16eb55825f7d6
Isabella Marquez <ilm3@illinois.edu>
2025-08-10 14:41:06 -0500

    fixes
    fixes to tablet screen size

--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,7 @@ function useDeviceType() {
       const width = window.innerWidth;
       if (width < 768) {
         setDeviceType('mobile');
-      } else if (width >= 768 && width <= 1376) {
+      } else if (width >= 768 && width <= 1366) {
         setDeviceType('tablet');
       } else {
         setDeviceType('desktop');
